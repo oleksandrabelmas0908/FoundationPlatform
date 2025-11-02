754b52ddff64a9a03728e8a6acc2a0e8389099b4
oleksandrabelmas0908 <oleksandr.abelmas0908@gmail.com>
2025-11-02 18:08:01 +0100

    Created project structure

---
+++ backend/analitic_service/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI(debug=True, title="Analitic servicd")
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", port=8000, log_level="info")
